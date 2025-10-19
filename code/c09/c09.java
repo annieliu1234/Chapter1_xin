86e439ebec4ddf1b4835a69023bac407e536e7fb
annieliu1234 <icn18163@gmail.com>
2025-10-19 20:42:15 +0800

    Initial commit: upload Java files

---
+++ code/c09/c09.java
@@ -0,0 +1,9 @@
+
+public class c09
+{
+    public static void main(String[] args)
+    {
+        System.out.print("歡迎使用Java!");
+        System.out.print("歡迎使用Java吧!");
+    }
+}
---
+++ code/c26/c26.java
@@ -0,0 +1,10 @@
+public class c26
+{
+    public static void main(String[] args)
+    {
+        int num;
+        num = 3;
+        
+        System.out.println("變數num的值是" + num);
+    }
+}
